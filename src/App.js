d92baff20c30926e7f33341e3edd1465066de1ac
tuecd <ductue2412@gmail.com>
2022-10-24 10:49:39 +0700

    feat: lan1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Nike from "../src/image/nike.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="card__">
+        <span className="like"></span>
+        <span className="cart"></span>
+      </div>
+
+      <div className="card__img d-flex">
+        <img src={Nike} alt="loading..." />
+      </div>
+      <h2 className="card__title">Nike Zoom KD 12</h2>
+        <p className="card__price">$99</p>
+
+      <div className="card__size">
+        <h3>Size:</h3>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+      </div>
+
+      <div className="card__color">
+        <h3>Color:</h3>
+        <span className="green"></span>
+        <span className="red"></span>
+        <span className="black"></span>
+      </div>
+
+      <div className="card__action">
+        <button>Buy Now</button>
+        <button>Add Card</button>
+      </div>
     </div>
   );
 }
